038fc1f10e483a89196ee1923788bcecae2c1aac
Mauricio Quijada <majiqtresi@gmail.com>
2016-10-04 00:50:45 -0500

    Modificando paquete r2d2 para ModelCar

--- r2d2/src/state_publisher.cpp
+++ r2d2/src/state_publisher.cpp
@@ -1,67 +1,111 @@
-#include <string>
 #include <ros/ros.h>
-#include <sensor_msgs/JointState.h>
 #include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+
+
+double Vx  = 0;
+double Vy  = 0;
+double Vth = 0;
+
+//*****************************************************************************************
+//float speed=.1;
+void callbackSpeed(const geometry_msgs::Twist::ConstPtr& msg)
+{
+     ROS_INFO("I heard Vel: linear[%lf %lf %lf]  angular[%lf %lf %lf]",
+           msg->linear.x, msg->linear.y, msg->linear.z,
+           msg->angular.x, msg->angular.y, msg->angular.z);
+
+  Vx  = msg->linear.x;
+  Vy  = msg->linear.y;
+  Vth = msg->angular.z;   
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "state_publisher");
+    
     ros::NodeHandle n;
-    ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1);
     tf::TransformBroadcaster broadcaster;
-    ros::Rate loop_rate(30);
+
+    ros::Subscriber vel_sub = n.subscribe("/hardware/speed", 1, callbackSpeed);
+
+    // ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    double x  = 0.0;
+    double y  = 0.0;
+    double th = 0.0;
+    
+    ros::Time current_time, last_time;
+    current_time = ros::Time::now();
+    last_time = ros::Time::now();
+    
+    ros::Rate r(30);
 
     const double degree = M_PI/180;
 
-    // robot state
+    
     double tilt = 0, tinc = degree, swivel=0, angle=0, height=0, hinc=0.005;
 
-    // message declarations
-    geometry_msgs::TransformStamped odom_trans;
-    sensor_msgs::JointState joint_state;
-    odom_trans.header.frame_id = "map";
-    odom_trans.child_frame_id = "base_link";
+    
 
     while (ros::ok()) {
-        //update joint_state
-        joint_state.header.stamp = ros::Time::now();
-        joint_state.name.resize(3);
-        joint_state.position.resize(3);
-        joint_state.name[0] ="swivel";
-        joint_state.position[0] = swivel;
-        joint_state.name[1] ="tilt";
-        joint_state.position[1] = tilt;
-        joint_state.name[2] ="periscope";
-        joint_state.position[2] = height;
+        ros::spinOnce();               // check for incoming messages
+        current_time = ros::Time::now();
 
+        //compute odometry in a typical way given the velocities of the robot
+        double dt = (current_time - last_time).toSec();
+        double delta_x  = (Vx * cos(th) - Vy * sin(th)) * dt;
+        double delta_y  = (Vx * sin(th) + Vy * cos(th)) * dt;
+        double delta_th = Vth * dt;
 
-        // update transform
-        // (moving in a circle with radius=2)
-        //for(odom_trans.transform.translation.x=0;odom_trans.transform.translation.x<10;odom_trans.transform.translation.x++ )
-        odom_trans.header.stamp = ros::Time::now();
-        odom_trans.transform.translation.x = cos(angle)*10;
-        odom_trans.transform.translation.y = sin(angle)*0;
-        odom_trans.transform.translation.z = .7;
-        odom_trans.transform.rotation=tf::createQuaternionMsgFromYaw(angle+M_PI/2);
-        //odom_trans.transform.rotation.x = 0;//
-        //odom_trans.transform.rotation.y=0;
-        //odom_trans.transform.rotation.z=swivel;
-        //odom_trans.transform.rotation.w=1;
-        //send the joint state and transform
-        //joint_pub.publish(joint_state);
+        x  += delta_x;
+        y  += delta_y;
+        th += delta_th;
+
+        //since all odometry is 6DOF we'll need a quaternion created from yaw
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+        //first, we'll publish the transform over tf
+        geometry_msgs::TransformStamped odom_trans;
+        odom_trans.header.stamp = current_time;
+        odom_trans.header.frame_id = "odom";
+        odom_trans.child_frame_id = "base_link";
+
+        odom_trans.transform.translation.x = x;
+        odom_trans.transform.translation.y = y;
+        odom_trans.transform.translation.z = 0.0;
+        odom_trans.transform.rotation      = odom_quat;
+
+        //send the odometry transform
         broadcaster.sendTransform(odom_trans);
 
-        // Create new robot state
-        tilt += tinc;
-        if (tilt<-.5 || tilt>0) tinc *= -1;
-        height += hinc;
-        if (height>.2 || height<0) hinc *= -1;
-        swivel += degree;
-        angle += degree/4;
+        //now send the transform from base_link to camera_link
+        broadcaster.sendTransform(
+        tf::StampedTransform(
+            tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
+                current_time,"base_link", "camera_link"));
 
-        // This will adjust as needed per iteration
-        loop_rate.sleep();
+        //Now create the odometry message
+        nav_msgs::Odometry odom;
+        odom.header.stamp = current_time;
+        odom.header.frame_id = "odom";
+
+        //set the position
+        odom.pose.pose.position.x  = x;
+        odom.pose.pose.position.y  = y;
+        odom.pose.pose.position.z  = 0.0;
+        odom.pose.pose.orientation = odom_quat;
+        //set the velocity
+
+        odom.child_frame_id = "base_link";
+        odom.twist.twist.linear.x  = Vx;
+        odom.twist.twist.linear.y  = Vy;
+        odom.twist.twist.angular.z = Vth;
+
+        //publish the odometry message
+        odom_pub.publish(odom);
+
+        last_time = current_time;
+        r.sleep();
     }
-
-
-    return 0;
 }
